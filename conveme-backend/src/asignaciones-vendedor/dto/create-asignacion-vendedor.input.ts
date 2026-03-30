0440e9d4c240694a51f9facac9ce3aebae6d7605
ivan1004-pixel <turtlesninjas10@gmail.com>
2026-03-30 17:31:36 -0600

    ya funciono el modulo de cortes al 100 .

--- conveme-backend/src/asignaciones-vendedor/dto/create-asignacion-vendedor.input.ts
+++ conveme-backend/src/asignaciones-vendedor/dto/create-asignacion-vendedor.input.ts
@@ -2,9 +2,14 @@ import { InputType, Field, Int } from '@nestjs/graphql';
 
 @InputType()
 export class CreateDetAsignacionInput {
-    // 👇 ESTO ES LO QUE FALTABA PARA QUE GRAPHQL DEJE PASAR EL ID 👇
+    // Ya lo teníamos opcional, ¡perfecto!
     @Field(() => Int, { nullable: true })
     id_det_asignacion?: number;
+
+    // 👇 AQUÍ ESTÁ EL TRUCO: Agregamos el asignacion_id como opcional
+    // Para que cuando el Frontend lo mande al editar, GraphQL no se asuste.
+    @Field(() => Int, { nullable: true })
+    asignacion_id?: number;
 
     @Field(() => Int)
     producto_id: number;
